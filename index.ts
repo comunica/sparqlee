d18c0acf645c74e74491ab747ce289c5d97cc838
Jitse De Smet <jitse@reedonk8.be>
2021-08-04 16:27:31 +0200

    fix backwards compatibility

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-export { AsyncEvaluator, IAsyncEvaluatorConfig } from './lib/evaluators/AsyncEvaluator';
-export { SyncEvaluator, ISyncEvaluatorConfig } from './lib/evaluators/SyncEvaluator';
+// TODO: this form is deprecated, we should not rename these types. Should change in next mayor update.
+export { AsyncEvaluator, IAsyncEvaluatorConfig as AsyncEvaluatorConfig } from './lib/evaluators/AsyncEvaluator';
+export { SyncEvaluator, ISyncEvaluatorConfig as SyncEvaluatorConfig } from './lib/evaluators/SyncEvaluator';
 export { AggregateEvaluator } from './lib/evaluators/AggregateEvaluator';
 
 export { ExpressionError, isExpressionError } from './lib/util/Errors';
