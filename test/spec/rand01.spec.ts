697b7f6088f34faf5c6db9eaf0ab468dd2009ea5
Jitse De Smet <jitse@reedonk8.be>
2021-08-05 16:42:25 +0200

    Rebase

--- test/spec/rand01.spec.ts
+++ test/spec/rand01.spec.ts
@@ -1,12 +1,37 @@
-import { aliases as a, testAll } from '../util/utils';
-import * as Data from './_data';
+import { bool } from '../util/Aliases';
+import { Notation } from '../util/TruthTable';
+import type { ITestArgumentBase } from '../util/utils';
+import { runTestTable } from '../util/utils';
 
 describe('We should respect the rand01 spec', () => {
-  testAll([
-    'DATATYPE(RAND()) = http://www.w3.org/2001/XMLSchema#double',
-    `RAND() >= 0.0 = ${a.true}`,
-    `RAND() < 1.0 = ${a.true}`,
-  ]);
+  const config: ITestArgumentBase = {
+    arity: 2,
+    notation: Notation.Infix,
+    aliases: bool,
+    operation: '',
+  };
+  runTestTable({
+    ...config,
+    operation: '>=',
+    testTable: `
+      RAND() 0.0 = true
+    `,
+  });
+  runTestTable({
+    ...config,
+    operation: '<',
+    testTable: `
+      RAND() 1.0 = true    
+    `,
+  });
+  runTestTable({
+    arity: 1,
+    operation: 'DATATYPE',
+    notation: Notation.Function,
+    testTable: `
+      RAND() = http://www.w3.org/2001/XMLSchema#double    
+    `,
+  });
 });
 
 /**
